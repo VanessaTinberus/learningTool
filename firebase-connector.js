a6f7b49c0029f9e4bfb7d0297642c7d92f07ae95
VanessaTinberus <42710894+VanessaTinberus@users.noreply.github.com>
2021-07-19 01:15:50 +0800

    Update firebase-connector.js

--- firebase-connector.js
+++ firebase-connector.js
@@ -1,4 +1,22 @@
-  // Your web app's Firebase configuration
+//variables
+let myFlashcards=[ 
+{
+    'question':'Does the sun rise in the east?',
+    'answer':'Yes.'
+},
+{
+    'question':'Is New York the capital of the U.S.A?',
+    'answer':'No.'
+},
+{
+    'question':'How many days are there in March?',
+    'answer':'31.'
+}
+];
+
+  
+
+// Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     apiKey: "AIzaSyCeFosFcyyPNKGUEz418IyC81MuU8InTPk",
@@ -16,23 +34,7 @@
 
 
 
-
-//variables
-let myFlashcards=[ 
-{
-    'question':'Does the sun rise in the east?',
-    'answer':'Yes.'
-},
-{
-    'question':'Is New York the capital of the U.S.A?',
-    'answer':'No.'
-},
-{
-    'question':'How many days are there in March?',
-    'answer':'31.'
-}
-];
-
+firebase.database().ref();  // Specifies the database root
 
 
 // Make the database point to the location root -> deck -> flashcards
