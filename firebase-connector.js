8a7af9b51104f98bff0a9d3b663cddd7700959ec
VanessaTinberus <42710894+VanessaTinberus@users.noreply.github.com>
2021-07-18 17:32:51 +0800

    Add files via upload

---
+++ firebase-connector.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyCeFosFcyyPNKGUEz418IyC81MuU8InTPk",
+    authDomain: "best-learning-tool-b4a83.firebaseapp.com",
+    projectId: "best-learning-tool-b4a83",
+    storageBucket: "best-learning-tool-b4a83.appspot.com",
+    messagingSenderId: "88761799029",
+    appId: "1:88761799029:web:64dd8e3001c4c197b1033c",
+    measurementId: "G-F288JZHDS9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
